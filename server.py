f48ea0e554b7fc598fe7d375a9cc25536b56b197
Akame <matthew70306@gmail.com>
2022-12-29 02:35:57 +0900

    update path

--- server.py
+++ server.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def main():
     return """<h1>逢甲大學圖書薦購系統</h1><a href="/se_final"><button>NID登入</button></a>"""
 
-@app.route("/")
+@app.route("/se_final")
 def login():
-    return redirect("https://judge.akameowo.ml/nid_login")
+    return redirect("https://judge.akameowo.ml/nid_login?forSe=1")
 
 @app.route("/result")
 def result():
